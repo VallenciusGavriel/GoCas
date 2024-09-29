600765f74fa25fce42782efcc663605ea0ef788d
Vallencius Gavriel <vallencius.siswanto@mobilepulsa.com>
2024-09-29 21:14:34 +0700

    migrations

---
+++ database/migrations/2024_09_29_140010_create_locations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('address_detail');
+            $table->string('province', 25);
+            $table->string('operational_hours', 25);
+            $table->string('longitude', 15);
+            $table->string('latitude', 15);
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
