52745f6f02e1773b75d9761ad287efb4e056db9a
kevinsorensen523 <kevinsorensen523@gmail.com>
2024-09-30 22:16:42 +0700

    Add Navbar and Footer Raw Component

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+    return (
+        <div>
+            <Box p={5} backgroundColor="darkblue" bottom={0}>
+                <Text color="white">Ini Footer</Text>
+            </Box>
+        </div>
+    );
+};
+
+export default Footer;
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Box, Heading, Button } from "@chakra-ui/react";
+import Footer from "@/Components/Footer";
+import Navbar from "@/Components/Navbar";
 
 const Home = () => {
     return (
-        <Box p={5}>
-            <Heading mb={4}>Welcome to My Laravel Inertia App</Heading>
-            <Button colorScheme="teal">Click Me</Button>
-        </Box>
+        <>
+            <Navbar />
+            <Box p={5}>
+                <Heading mb={4}>Welcome to My Laravel Inertia App</Heading>
+                <Button colorScheme="teal">Click Me</Button>
+            </Box>
+            <Footer />
+        </>
     );
 };
 
