ec5468b1d972c59815c181c749880f25c385ce11
kevinsorensen523 <kevinsorensen523@gmail.com>
2024-10-18 00:42:10 +0700

    90% Done

--- resources/js/Components/Navbar/Navbar.jsx
+++ resources/js/Components/Navbar/Navbar.jsx
@@ -1,48 +1,232 @@
-import React from "react";
-import { Flex, Box, Image, Button, Text } from "@chakra-ui/react";
-import { Link } from "@inertiajs/react";
-import GocasLogo from "../../../../public/images/navbar/gocas-logo.png"; // Make sure the path is correct
+import React, { useState } from "react";
+import {
+    Box,
+    Flex,
+    Image,
+    Text,
+    Button as ChakraButton,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    IconButton,
+    Collapse,
+    useDisclosure,
+    useBreakpointValue,
+    Link,
+} from "@chakra-ui/react";
+import { ChevronDownIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { motion } from "framer-motion";
+import GocasLogo from "../../../../public/images/navbar/gocas-logo.png"; // Ensure correct path
+
+// Import images
+import IDFlag from "../../../../public/images/navbar/IDFlag.png";
+import ENFlag from "../../../../public/images/navbar/ENFlag.png";
+
+const MotionBox = motion(Box);
+const MotionText = motion(Text);
 
 const Navbar = () => {
+    const { isOpen, onToggle } = useDisclosure();
+    const display = useBreakpointValue({ base: "none", xl: "flex" });
+
+    const isActive = (route) => window.location.pathname === route;
+
+    const [selectedLanguage, setSelectedLanguage] = useState("EN");
+
+    const handleLanguageChange = (language) => {
+        setSelectedLanguage(language);
+    };
+
+    // Update these links to reflect your application's pages
+    const menuLinks = [
+        { name: "Lokasi", href: "/" },
+        { name: "Produk", href: "/products" },
+        { name: "Pengguna", href: "/users" },
+        { name: "Partner", href: "/partners" },
+        { name: "Tentang Kami", href: "/about" },
+    ];
+
+    // Update these buttons if needed
+    const buttons = [
+        { label: "Masuk", variant: "solid", id: "signInButton" },
+        { label: "Daftar", variant: "outline", id: "registerButton" },
+    ];
+
     return (
-        <>
+        <MotionBox
+            p={2}
+            bg="white"
+            shadow="md"
+            borderBottom="1px"
+            borderColor="gray.200"
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            width="100%"
+            zIndex="20"
+            borderRadius="xl"
+        >
             <Flex
-                as="nav"
                 align="center"
                 justify="space-between"
-                padding="4"
-                bg="#F2F2F3"
-                boxShadow="sm"
+                maxW="1200px"
+                mx="auto"
             >
-                <Flex align="center">
-                    <Image src={GocasLogo} alt="GoCas Logo" mr={4} />
+                {/* Logo Section */}
+                <Flex align="center" justify="flex-start">
+                    <Image
+                        src={GocasLogo}
+                        alt="GoCas Logo"
+                        boxSize="80px"
+                        mr={4}
+                        objectFit="contain"
+                    />
+
+                    {/* Language Dropdown */}
+                    <Box
+                        ml={10}
+                        borderWidth="1px"
+                        borderRadius="md"
+                        px={2}
+                        display="flex"
+                        alignItems="center"
+                        borderColor="black"
+                    >
+                        <Flex align="center">
+                            <Image
+                                src={
+                                    selectedLanguage === "ID" ? IDFlag : ENFlag
+                                }
+                                alt={
+                                    selectedLanguage === "ID"
+                                        ? "Indonesian Flag"
+                                        : "English Flag"
+                                }
+                                boxSize="16px"
+                                mr={2}
+                            />
+                            <Text fontWeight="bold">{selectedLanguage}</Text>
+                        </Flex>
+                        <Menu>
+                            <MenuButton
+                                as={IconButton}
+                                icon={<ChevronDownIcon />}
+                                ml={2}
+                                aria-label="Select Language"
+                                bg="transparent"
+                                _hover={{ bg: "transparent" }}
+                                _active={{ bg: "transparent" }}
+                            />
+                            <MenuList>
+                                <MenuItem
+                                    onClick={() => handleLanguageChange("ID")}
+                                >
+                                    <Image
+                                        src={IDFlag}
+                                        alt="Indonesian Flag"
+                                        boxSize="20px"
+                                        mr={2}
+                                    />
+                                    <Text>Bahasa Indonesia (ID)</Text>
+                                </MenuItem>
+                                <MenuItem
+                                    onClick={() => handleLanguageChange("EN")}
+                                >
+                                    <Image
+                                        src={ENFlag}
+                                        alt="English Flag"
+                                        boxSize="20px"
+                                        mr={2}
+                                    />
+                                    <Text>English (EN)</Text>
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Box>
                 </Flex>
 
-                <Flex gap="4" align="center">
-                    <Link to="/lokasi">
-                        <Text>Lokasi</Text>
-                    </Link>
-                    <Link to="/produk">
-                        <Text>Produk</Text>
-                    </Link>
-                    <Link to="/pengguna">
-                        <Text>Pengguna</Text>
-                    </Link>
-                    <Link to="/partner">
-                        <Text>Partner</Text>
-                    </Link>
-                    <Link to="/tentang-kami">
-                        <Text>Tentang Kami</Text>
-                    </Link>
-                    <Button colorScheme="green" size="sm">
-                        Masuk
-                    </Button>
-                    <Button colorScheme="yellow" size="sm">
-                        Daftar
-                    </Button>
+                {/* Hamburger Icon for Mobile */}
+                <IconButton
+                    display={{ base: "flex", xl: "none" }}
+                    icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                    onClick={onToggle}
+                    aria-label="Toggle Navigation"
+                />
+
+                {/* Desktop Menu */}
+                <Flex align="center" ml={10} spacing={8} display={display}>
+                    {menuLinks.map((link) => (
+                        <Link key={link.name} href={link.href}>
+                            <MotionText
+                                fontFamily="poppins"
+                                mx={4}
+                                cursor="pointer"
+                                fontWeight={
+                                    isActive(link.href) ? "bold" : "normal"
+                                }
+                                color="black"
+                                transition={{ duration: 0.3 }}
+                            >
+                                {link.name}
+                            </MotionText>
+                        </Link>
+                    ))}
+                </Flex>
+
+                {/* Button Section */}
+                <Flex display={display}>
+                    {buttons.map((button) => (
+                        <ChakraButton
+                            key={button.id}
+                            variant={button.variant}
+                            mr={4}
+                            borderRadius="xl"
+                        >
+                            {button.label}
+                        </ChakraButton>
+                    ))}
                 </Flex>
             </Flex>
-        </>
+
+            {/* Mobile Menu */}
+            <Collapse in={isOpen}>
+                <Box
+                    display={{ base: "block", lg: "none" }}
+                    p={4}
+                    bg="white"
+                    borderBottom="1px"
+                    borderColor="gray.200"
+                >
+                    <Flex direction="column" align="center">
+                        {menuLinks.map((link) => (
+                            <Link key={link.name} href={link.href}>
+                                <Text
+                                    my={2}
+                                    fontFamily="poppins"
+                                    cursor="pointer"
+                                >
+                                    {link.name}
+                                </Text>
+                            </Link>
+                        ))}
+                        <Flex direction="column" mt={4}>
+                            {buttons.map((button) => (
+                                <ChakraButton
+                                    key={button.id}
+                                    variant={button.variant}
+                                    mb={2}
+                                    borderRadius="xl"
+                                >
+                                    {button.label}
+                                </ChakraButton>
+                            ))}
+                        </Flex>
+                    </Flex>
+                </Box>
+            </Collapse>
+        </MotionBox>
     );
 };
 
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 import Footer from "@/Components/Footer/Footer";
+import Navbar from "@/Components/Navbar/Navbar";
 import { Head } from "@inertiajs/react";
 import {
     Home1,
@@ -11,7 +12,7 @@ import {
     Home6,
     Home7,
     Home8,
-} from "@/Components/Home"; // Adjust the path as necessary
+} from "@/Components/Home";
 
 const Home = () => {
     const homeComponents = [
@@ -27,46 +28,52 @@ const Home = () => {
 
     return (
         <>
+            <Navbar />
             <Head title="Home" />
-            {homeComponents.map((Component, index) => (
-                <Box
-                    key={index}
-                    w="full"
-                    h="screen"
-                    bgImage={`url('/images/home/background-${index + 1}.png')`}
-                    bgSize="cover"
-                    bgPos="center"
-                    p={10}
-                    borderRadius="0"
-                    position="relative"
-                    overflow="hidden"
-                >
-                    {index === 0 && (
-                        <>
-                            <Box
-                                position="absolute"
-                                bottom="0"
-                                left="0"
-                                right="0"
-                                height="30%"
-                                backgroundColor="rgba(255, 255, 255, 0.9)"
-                                zIndex="1"
-                            />
-                            <Box
-                                position="absolute"
-                                top="0"
-                                left="0"
-                                right="0"
-                                height="70%"
-                                backgroundColor="rgba(255, 255, 255, 0.45)"
-                                zIndex="1"
-                            />
-                        </>
-                    )}
-                    <Component />
-                </Box>
-            ))}
-
+            <VStack spacing={0} mt="64px">
+                {" "}
+                {/* Adjust the margin-top to match the Navbar's height */}
+                {homeComponents.map((Component, index) => (
+                    <Box
+                        key={index}
+                        w="full"
+                        h="screen"
+                        bgImage={`url('/images/home/background-${
+                            index + 1
+                        }.png')`}
+                        bgSize="cover"
+                        bgPos="center"
+                        p={10}
+                        borderRadius="0"
+                        position="relative"
+                        overflow="hidden"
+                    >
+                        {index === 0 && (
+                            <>
+                                <Box
+                                    position="absolute"
+                                    bottom="0"
+                                    left="0"
+                                    right="0"
+                                    height="30%"
+                                    backgroundColor="rgba(255, 255, 255, 0.9)"
+                                    zIndex="1"
+                                />
+                                <Box
+                                    position="absolute"
+                                    top="0"
+                                    left="0"
+                                    right="0"
+                                    height="70%"
+                                    backgroundColor="rgba(255, 255, 255, 0.45)"
+                                    zIndex="1"
+                                />
+                            </>
+                        )}
+                        <Component />
+                    </Box>
+                ))}
+            </VStack>
             <Footer />
         </>
     );
