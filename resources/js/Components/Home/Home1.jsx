0ff8b7b7a329b3a306d5863c8e7e1254f009d941
Vallencius Gavriel <vallencius.siswanto@mobilepulsa.com>
2025-01-16 06:36:39 +0700

    Max Width Home

--- resources/js/Components/Home/Home1.jsx
+++ resources/js/Components/Home/Home1.jsx
@@ -40,156 +40,166 @@ const fadeInRight = {
 const Home1 = () => {
     const { language } = useLanguage();
     return (
-        <Grid
-            templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
-            height="80%"
-            width="full"
-            zIndex="10"
+        <MotionBox
+            maxWidth="1500px"
+            mx="auto"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInUp}
+            transition={{ duration: 1 }}
         >
-            {/* Left Section */}
-            <MotionBox
-                display="flex"
-                flexDirection="column"
-                justifyContent={{ base: "center", md: "flex-end" }}
-                alignItems={{ base: "center", md: "flex-start" }}
-                textAlign={{ base: "center", md: "left" }}
-                p="4"
-                height="100%"
+            <Grid
+                templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+                height="80%"
+                width="full"
                 zIndex="10"
-                initial="hidden"
-                animate="visible"
-                variants={fadeInLeft}
-                transition={{ duration: 1 }}
             >
-                <HeaderText
-                    iconColor="brown"
-                    size="normal"
-                    withIcon={false}
-                    topMargin={40}
-                    inputclass={"xl:!text-5xl md:!text-3xl !text-lg"}
-                    px="0px"
+                {/* Left Section */}
+                <MotionBox
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent={{ base: "center", md: "flex-end" }}
+                    alignItems={{ base: "center", md: "flex-start" }}
+                    textAlign={{ base: "center", md: "left" }}
+                    p="4"
+                    height="100%"
+                    zIndex="10"
+                    initial="hidden"
+                    animate="visible"
+                    variants={fadeInLeft}
+                    transition={{ duration: 1 }}
                 >
-                    {languages[language]["headline_1"]}{" "}
-                    {/* Isi daya tanpa ribet */}
-                </HeaderText>
-                <HeaderText
-                    iconColor="brown"
-                    size="large"
-                    withIcon={false}
-                    topMargin={6}
-                    inputclass={"xl:!text-7xl md:!text-5xl !text-3xl"}
-                    px="0px"
-                >
-                    {languages[language]["headline_2"]} {/* kapanpun */}
-                </HeaderText>
-                <HeaderText
-                    iconColor="brown"
-                    size="large"
-                    withIcon={false}
-                    topMargin={6}
-                    inputclass={"xl:!text-7xl md:!text-5xl !text-3xl"}
-                    px="0px"
-                >
-                    {languages[language]["headline_3"]} {/* dimanapun */}
-                </HeaderText>
-                <Text
-                    mt={6}
-                    fontSize="lg"
-                    fontFamily="poppins"
-                    fontWeight="bold"
-                >
-                    {languages[language]["description"]}{" "}
-                    {/* GoCas Indonesia adalah brand penyewaan... */}
-                </Text>
-                <Link href="/location">
-                    <MotionButton
-                        bgGradient="linear(to-r, #6EA93C, #25893C)"
-                        color="white"
-                        _hover={{
-                            bgGradient: "linear(to-r, green.500, teal.600)",
-                        }}
-                        borderRadius="full"
+                    <HeaderText
+                        iconColor="brown"
+                        size="normal"
+                        withIcon={false}
+                        topMargin={40}
+                        inputclass={"xl:!text-5xl md:!text-3xl !text-lg"}
+                        px="0px"
+                    >
+                        {languages[language]["headline_1"]}{" "}
+                        {/* Isi daya tanpa ribet */}
+                    </HeaderText>
+                    <HeaderText
+                        iconColor="brown"
+                        size="large"
+                        withIcon={false}
+                        topMargin={6}
+                        inputclass={"xl:!text-7xl md:!text-5xl !text-3xl"}
+                        px="0px"
+                    >
+                        {languages[language]["headline_2"]} {/* kapanpun */}
+                    </HeaderText>
+                    <HeaderText
+                        iconColor="brown"
+                        size="large"
+                        withIcon={false}
+                        topMargin={6}
+                        inputclass={"xl:!text-7xl md:!text-5xl !text-3xl"}
+                        px="0px"
+                    >
+                        {languages[language]["headline_3"]} {/* dimanapun */}
+                    </HeaderText>
+                    <Text
+                        mt={6}
                         fontSize="lg"
                         fontFamily="poppins"
                         fontWeight="bold"
-                        px={8}
-                        py={6}
-                        mt={{ base: 6, md: 12 }}
-                        mb={{ base: 10, md: 28 }}
-                        boxShadow="md"
-                        w={{ base: 60, md: "full" }}
-                        whileHover={{ scale: 1.1 }}
-                        transition={{ duration: 0.3 }}
                     >
-                        <Text
+                        {languages[language]["description"]}{" "}
+                        {/* GoCas Indonesia adalah brand penyewaan... */}
+                    </Text>
+                    <Link href="/location">
+                        <MotionButton
+                            bgGradient="linear(to-r, #6EA93C, #25893C)"
+                            color="white"
+                            _hover={{
+                                bgGradient: "linear(to-r, green.500, teal.600)",
+                            }}
+                            borderRadius="full"
+                            fontSize="lg"
                             fontFamily="poppins"
-                            className={"md:text-lg text-xs"}
+                            fontWeight="bold"
+                            px={8}
+                            py={6}
+                            mt={{ base: 6, md: 12 }}
+                            mb={{ base: 10, md: 28 }}
+                            boxShadow="md"
+                            w={{ base: 60, md: "full" }}
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: 0.3 }}
                         >
-                            {languages[language]["button_text"]}{" "}
-                            {/* Temukan GoCas terdekat */}
-                        </Text>
-                    </MotionButton>
-                </Link>
-            </MotionBox>
+                            <Text
+                                fontFamily="poppins"
+                                className={"md:text-lg text-xs"}
+                            >
+                                {languages[language]["button_text"]}{" "}
+                                {/* Temukan GoCas terdekat */}
+                            </Text>
+                        </MotionButton>
+                    </Link>
+                </MotionBox>
 
-            {/* Right Section */}
-            <MotionBox
-                position={{ base: "relative", md: "absolute" }}
-                right={{ md: "0" }}
-                bottom={{ md: "0" }}
-                p={{ base: "4", md: "28" }}
-                width={{ base: "100%", md: "auto" }}
-                initial="hidden"
-                animate="visible"
-                variants={fadeInRight}
-                transition={{ duration: 1 }}
-            >
-                <HStack
-                    justifyContent={{ base: "space-evenly", md: "flex-end" }}
-                    spacing={4}
+                {/* Right Section */}
+                <MotionBox
+                    position={{ base: "relative", md: "absolute" }}
+                    right={{ md: "0" }}
+                    bottom={{ md: "0" }}
+                    p={{ base: "4", md: "28" }}
+                    width={{ base: "100%", md: "auto" }}
+                    initial="hidden"
+                    animate="visible"
+                    variants={fadeInRight}
+                    transition={{ duration: 1 }}
                 >
-                    <VStack>
-                        <Link
-                            href={
-                                "https://play.google.com/store/apps/details?id=so.dian.gocasid"
-                            }
-                        >
-                            <MotionImage
-                                src={GPlay}
-                                alt="GoCas - Sewa Powerbank Murah dan Cepat di Indonesia"
-                                mb={2}
-                                w={"200px"}
-                                objectFit="contain"
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.3 }}
-                            />
-                        </Link>
-                        <Link
-                            href={
-                                "https://apps.apple.com/id/app/gocas/id6738688391?l=id"
-                            }
-                        >
-                            <MotionImage
-                                src={AppStore}
-                                alt="GoCas - Sewa Powerbank Murah dan Cepat di Indonesia"
-                                w={"200px"}
-                                objectFit="contain"
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.3 }}
-                            />
-                        </Link>
-                    </VStack>
-                    <MotionImage
-                        src={QRCode}
-                        alt="QR Code"
-                        boxSize={{ base: "100px", md: "150px" }}
-                        objectFit="contain"
-                        whileHover={{ rotate: 360 }}
-                        transition={{ duration: 0.5 }}
-                    />
-                </HStack>
-            </MotionBox>
-        </Grid>
+                    <HStack
+                        justifyContent={{ base: "space-evenly", md: "flex-end" }}
+                        spacing={4}
+                    >
+                        <VStack>
+                            <Link
+                                href={
+                                    "https://play.google.com/store/apps/details?id=so.dian.gocasid"
+                                }
+                            >
+                                <MotionImage
+                                    src={GPlay}
+                                    alt="GoCas - Sewa Powerbank Murah dan Cepat di Indonesia"
+                                    mb={2}
+                                    w={"200px"}
+                                    objectFit="contain"
+                                    whileHover={{ scale: 1.1 }}
+                                    transition={{ duration: 0.3 }}
+                                />
+                            </Link>
+                            <Link
+                                href={
+                                    "https://apps.apple.com/id/app/gocas/id6738688391?l=id"
+                                }
+                            >
+                                <MotionImage
+                                    src={AppStore}
+                                    alt="GoCas - Sewa Powerbank Murah dan Cepat di Indonesia"
+                                    w={"200px"}
+                                    objectFit="contain"
+                                    whileHover={{ scale: 1.1 }}
+                                    transition={{ duration: 0.3 }}
+                                />
+                            </Link>
+                        </VStack>
+                        <MotionImage
+                            src={QRCode}
+                            alt="QR Code"
+                            boxSize={{ base: "100px", md: "150px" }}
+                            objectFit="contain"
+                            whileHover={{ rotate: 360 }}
+                            transition={{ duration: 0.5 }}
+                        />
+                    </HStack>
+                </MotionBox>
+            </Grid>
+        </MotionBox>
     );
 };
 
